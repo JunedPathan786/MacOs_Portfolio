4c6a20992afaca3521b00231b045d37d0f6c8bf1
unknown <junedp068@gmail.com>
2026-08-16 10:50:37 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import dayjs from 'dayjs'
+import { navLinks, navIcons } from '#constants'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className='font-bold'>Juned's Portfolio</p>
+
+        <ul>
+          {
+            navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))
+          }
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {
+            navIcons.map(({id, img}) => (
+              <li key={id}>
+                <img src={img} className='icons-hover' alt={`icons-{id}`} />
+              </li>
+            ))
+          }
+        </ul>
+         <time>
+          {dayjs().format("ddd MMM D h:mm A")}
+        </time>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+const navLinks = [
+  { id: 1, name: "Projects" },
+  { id: 2, name: "Skill" },
+  { id: 3, name: "Experience" },
+  { id: 4, name: "Resume" },
+]
+
+const navIcons = [
+  { id: 1, img: "/icons/wifi.svg"},
+  { id: 1, img: "/icons/search.svg"},
+  { id: 1, img: "/icons/user.svg"},
+  { id: 1, img: "/icons/mode.svg"},
+]
+
+export { navLinks, navIcons };
